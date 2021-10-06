d90a98f5e573bcb15f3031d9ad3ce97dff78ba92
Cynthia Onwuana <conwuana1@yahoo.com>
2021-10-06 14:01:15 -0400

    Created form for posts

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.http.response import HttpResponseRedirect
+from django.shortcuts import render, reverse
+from posts.forms import AddPostForm
+from posts.models import Post
+from django.contrib.auth.models import User
 
 # Create your views here.
+def index(request):
+    posts = Post.objects.all()
+    return render(request, 'index.html', {'posts': posts})
+
+def addPost(request):
+    if request.method == 'POST':
+        form = AddPostForm(request.POST)
+        if form.is_valid():
+            data = form.cleaned_data
+            Post.objects.create(
+                user=request.user,
+                body=data['body'],
+                # time_created=data['time_created']
+            )
+        return HttpResponseRedirect(reverse('home'))
+    form = AddPostForm()
+    return render(request, 'generic_form.html', {'form': form})

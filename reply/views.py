917b408cba18dd58b352fd7ffbf0b9dbb54857ae
graeberj <graeberj88@gmail.com>
2021-10-13 06:53:31 -0700

    clickable links for replies and posts

--- reply/views.py
+++ reply/views.py
@@ -8,7 +8,11 @@ from django.views.generic import View
 
 class ReplyDetailView(View):
     def get(self, request, reply_id):
-        template_name = "reply_detail.html"
-        reply = Reply.objects.filter(id=reply_id).first()
-        context = {"reply": reply}
+        template_name = "post_detail.html"
+        reply = Reply.objects.get(id=reply_id)
+        replies = reply.get_descendants(include_self=False)
+        context = {
+                    "reply": reply,
+                    "replies": replies
+                    }
         return render(request, template_name, context)

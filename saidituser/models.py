110922cf5142ab23ccdca9979b85fbd64abbc0dd
graeberj <graeberj88@gmail.com>
2021-10-14 09:06:19 -0700

    merge

--- saidituser/models.py
+++ saidituser/models.py
@@ -1,13 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-from django.db.models.fields.related import ManyToManyField
 
 
 class SaidItUser(AbstractUser):
     display_name = models.CharField(max_length=30)
     age = models.IntegerField(null=True, blank=True)
     bio = models.TextField(null=True, blank=False)
-    member = models.ManyToManyField('self', blank=True, related_name='group_member')
+    
     following = models.ManyToManyField("self", blank =True, related_name="followers", symmetrical=False) # noqa
     # notifications
     # display picture
